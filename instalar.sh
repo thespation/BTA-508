8728b466f066666f0714f6edd220c81e46c79d54
William Santos <thespation@gmail.com>
2022-08-29 23:04:24 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -9,13 +9,13 @@ VERD="\033[0;32m"	#Deixa a saída na cor verde
 CIAN="\033[0;36m"	#Deixa a saída na cor ciano
 NORM="\033[0m"		#Volta para a cor padrão
 # Alias firmware
-SUDC='sudo cp -iv' 													#Comando para cópia de arquivo
+SUDC='sudo cp -iv' 							#Comando para cópia de arquivo
 PTMP='20201202_LINUX_BT_DRIVER/rtkbt-firmware/lib/firmware/rtlbt/'	#Pasta temporária
-PF1='/lib/firmware/rtl_bt/rtl8761b_fw.bin'							#Pasta e arquivo de firmware
-PF2='/lib/firmware/rtl_bt/rtl8761b_config.bin'						#Pasta e arquivo de firmware
-PF='/lib/firmware/rtl_bt/'											#Pasta de firmware
-ARQ1='rtl8761b_fw'													#Arquivo de firmware 	1
-ARQ2='rtl8761b_config'												#Arquivo de firmware 	2
+PF1='/lib/firmware/rtl_bt/rtl8761b_fw.bin'				#Pasta e arquivo de firmware
+PF2='/lib/firmware/rtl_bt/rtl8761b_config.bin'				#Pasta e arquivo de firmware
+PF='/lib/firmware/rtl_bt/'						#Pasta de firmware
+ARQ1='rtl8761b_fw'							#Arquivo de firmware 	1
+ARQ2='rtl8761b_config'							#Arquivo de firmware 	2
 #Alias verificação de kernel
 UNA='uname -v'
 
